9bbc24bac4ea627757245cad8b530455326d7dd6
Antonio Fernando <antoniofernandodj@outlook.com>
2024-02-20 17:57:19 -0300

    *

--- src/domain/models/user.js
+++ src/domain/models/user.js
@@ -1,4 +1,4 @@
-class User {
+export class User {
     constructor(id, username, senha) {
         this.id = id
         this.username = username
---
+++ teste.js
@@ -0,0 +1,5 @@
+import { User } from "./src/domain/models/user.js";
+
+let pessoa = new User('1', 'ricardo', '123456')
+
+console.log({pessoa: pessoa})
