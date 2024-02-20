3167e9d9bdd156b55708823b507d34102207fb55
Antonio Fernando <antoniofernandodj@outlook.com>
2024-02-20 19:37:45 -0300

    *

--- src/routers/login.js
+++ src/routers/login.js
@@ -2,23 +2,6 @@ import express from 'express';
 import { UserRepository } from '../domain/repositories/userRepository.js';
 
 export const loginRouter = express.Router();
-const userRepository = new UserRepository();
-
-
-loginRouter.get('/user/:id', async (req, res) => {
-    const userId = req.params.id;
-    try {
-        const user = await userRepository.getOne(userId);
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
-    } catch (error) {
-        console.error('Error fetching user:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-});
 
 async function authenticateUser(headers) {
     console.log({headers: headers})
@@ -29,7 +12,23 @@ async function authenticateUser(headers) {
     return user
 }
 
-loginRouter.get('/home', async(req, res) => {
+const getUser = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const userRepository = new UserRepository();
+        const user = await userRepository.getOne(userId);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const getHome = async(req, res) => {
     let user = await authenticateUser(req.headers)
     if (!user) {
         res.status(401).json({ message: 'Acesso não autorizado: Login requerido' });
@@ -39,35 +38,35 @@ loginRouter.get('/home', async(req, res) => {
         }
         res.status(200).json(body);
     }
-})
+}
 
-
-loginRouter.get('/user/', async (req, res) => {
+const getAllUsers = async (req, res) => {
     try {
+        const userRepository = new UserRepository();
         const users = await userRepository.getAll();
         res.json(users);
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+}
 
-
-loginRouter.post('/user', async (req, res) => {
+const registerUser = async (req, res) => {
     const newUser = req.body;
     try {
+        const userRepository = new UserRepository();
         await userRepository.save(newUser);
         res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+}
 
-
-loginRouter.post('/login-user', async (req, res) => {
+const loginUser = async (req, res) => {
     const data = req.body;
     try {
+        const userRepository = new UserRepository();
         let user = await userRepository.getOneByUsername(data.username);
         // let token = generateJWTToken(user)
         if (user.senha == data.senha) {
@@ -78,13 +77,13 @@ loginRouter.post('/login-user', async (req, res) => {
         console.error('Error creating user:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+}
 
-
-loginRouter.put('/user/:id', async (req, res) => {
+const updateUser = async (req, res) => {
     const userId = req.params.id;
     const newData = req.body;
     try {
+        const userRepository = new UserRepository();
         const updated = await userRepository.update(userId, newData);
         if (updated) {
             res.json({ message: 'User updated successfully' });
@@ -95,12 +94,12 @@ loginRouter.put('/user/:id', async (req, res) => {
         console.error('Error updating user:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+}
 
-
-loginRouter.delete('/user/:id', async (req, res) => {
+const deleteUser = async (req, res) => {
     const userId = req.params.id;
     try {
+        const userRepository = new UserRepository();
         const deleted = await userRepository.delete(userId);
         if (deleted) {
             res.json({ message: 'User deleted successfully' });
@@ -111,4 +110,13 @@ loginRouter.delete('/user/:id', async (req, res) => {
         console.error('Error deleting user:', error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+}
+
+
+loginRouter.get('/user/:id', getUser);
+loginRouter.get('/home', getHome)
+loginRouter.get('/user/', getAllUsers);
+loginRouter.post('/user', registerUser);
+loginRouter.post('/login-user', loginUser);
+loginRouter.put('/user/:id', updateUser);
+loginRouter.delete('/user/:id', deleteUser);
